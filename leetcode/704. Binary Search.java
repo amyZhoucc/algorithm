e4e7dd4232f956995bf5d7010ee5c6c12ac6875d
amyZhoucc <2917630971@qq.com>
2020-08-06 17:45:17 +0800

    fix a bug

--- leetcode/704. Binary Search.java
+++ leetcode/704. Binary Search.java
@@ -1,28 +1,18 @@
-class Solution {		// 暴力解，时间复杂度为O(n)
-    public int searchInsert(int[] nums, int target) {
-        int i;
-         for(i = 0;  i < nums.length; i++){
-             if(target <= nums[i])  return i;
-         }
-         return i;
-    }
-}
-
-class Solution {		// 改进后，用二分查找，时间复杂度为O(logn)
-    public int searchInsert(int[] nums, int target) {
-        int n = nums.length;    // 记录数组长度
-        int left = 0, right = n - 1, mid;   // 设置三个指针，代表左右中
-        int ans = n; 	// 答案初始化为n（关键），这样能防止因为插入的位置在最后而需要的额外判断
-        while(left <=  right){  // 循环条件
-            mid = ((right -  left)>>1) + left;	// 这样可以防止大数相加之后的溢出
-            if(target <= nums[mid]){
-                right = mid - 1;	// 边界向左收缩
-                ans = mid;			// 严格小于target的数，不可能是解，只有大于target的数才有可能，从而不断收缩
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid;
+        while(left <=  right){
+            mid = ((right - left)>> 1) + left;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                right = mid - 1;
             }
             else{
-                left = mid + 1;		// 小于target，直接边界向右收缩
+                left = mid + 1;
             }
-        }  
-        return ans;
+        }
+        return -1;
     }
 }
