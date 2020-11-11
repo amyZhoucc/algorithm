4a17741ff04e7ef7e1a6e32ac656f2893d1fa285
amyZhoucc <2917630971@qq.com>
2020-11-11 21:47:51 +0800

    update 121

--- leetcode/121. Best Time to Buy and Sell Stock.java
+++ leetcode/121. Best Time to Buy and Sell Stock.java
@@ -16,11 +16,26 @@ class Solution {		// 一次遍历的方法
         if(prices.length == 0) return 0;
         int maxpro = 0;     // 记录最大利润
         int minprice = prices[0];   // 存放历史最低值
-        for(int i = 1; i < prices.length; i++){         // 卖出时机
+        for(int i = 1; i < prices.length; i++){         // 遍历的描述是：卖出时机——今天卖出的最大价值是多少：就是今天的价格-之前的历史最低价
             minprice = minprice < prices[i-1] ? minprice : prices[i-1]; // 更新历史最低值
             int temp = prices[i] - minprice;
             maxpro = maxpro > temp ? maxpro : temp;
         }
         return maxpro;
+    }
+}
+
+class Solution {                // 动态规划的方法
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0) return 0;             // 排除边界条件
+        int minprice = prices[0];
+        int[]dp = new int[prices.length];           // 维护一个数组，存放当前天时利润的最大值
+        dp[0] = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            minprice = Math.min(prices[i], minprice);       // 求当前的历史最低值
+            dp[i] = Math.max(dp[i - 1], prices[i] - minprice);  // 求当前天的
+        }
+        return dp[prices.length - 1];
     }
 }
